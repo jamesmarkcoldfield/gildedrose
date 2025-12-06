7a8ef3b0c6997db958ed6d0a35d5e5690813e2e7
James Robertson <oatbake@hotmail.co.uk>
2025-12-06 17:11:38 +0000

    Added test helper and renamed existing unit test for clarity

--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -1,13 +1,14 @@
 ﻿using GildedRoseKata;
+using GildedRoseTests.Helpers;
 
 namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
     [Fact]
-    public void GildedRose_UpdateQuality_ItemNameNotChangedOnUpdate()
+    public void GildedRose_UpdateQuality_SingleItem_ItemNameNotChangedOnUpdate()
     {
-        List<Item> items = [ new Item { Name = "foo", SellIn = 0, Quality = 0 } ];
+        List<Item> items = [ItemHelpers.CreateItem("foo", 0, 0)];
         GildedRose app = new(items);
         app.UpdateQuality();
         Assert.Equal("foo", items[0].Name);
---
+++ src/GildedRoseTests/Helpers/ItemHelpers.cs
@@ -0,0 +1,12 @@
+﻿using GildedRoseKata;
+
+namespace GildedRoseTests.Helpers
+{
+    public static class ItemHelpers
+    {
+        public static Item CreateItem(string name, int sellIn, int quality)
+        {
+            return new Item { Name = name, SellIn = sellIn, Quality = quality };
+        }
+    }
+}
