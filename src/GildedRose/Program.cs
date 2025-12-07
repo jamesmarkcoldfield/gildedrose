b3d55e57830daed6cf1d9930aa0cca9ae8ef4333
James Robertson <oatbake@hotmail.co.uk>
2025-12-07 23:58:41 +0000

    Tidy up

--- src/GildedRose/Program.cs
+++ src/GildedRose/Program.cs
@@ -17,8 +17,6 @@ public class Program
             new StoreItem("Backstage passes to a TAFKAL80ETC concert", 15, 20),
             new StoreItem("Backstage passes to a TAFKAL80ETC concert", 10, 49),
             new StoreItem("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-
-            
             // this conjured item does not work properly yet
             new StoreItem("Conjured Mana Cake", 3, 6)
         ];
