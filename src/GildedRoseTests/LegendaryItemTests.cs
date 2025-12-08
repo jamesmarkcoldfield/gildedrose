c825085110c7b7526fed856ef4fbc0480bc8cf9e
James Robertson <oatbake@hotmail.co.uk>
2025-12-08 00:13:27 +0000

    Added tests for Legendary Item

---
+++ src/GildedRoseTests/LegendaryItemTests.cs
@@ -0,0 +1,65 @@
+﻿using GildedRoseKata.Items;
+
+namespace GildedRoseTests;
+
+public class LegendaryItemTests
+{
+
+    [Fact]
+    public void LegendaryItem_Constructor_NameAsSpecified()
+    {
+        // Arrange & Act
+        var legendaryItem = new LegendaryItem("A legendary item", 12);
+
+        // Assert
+        Assert.Equal("A legendary item", legendaryItem.Name);
+    }
+
+    [Fact]
+    public void LegendaryItem_Constructor_SellInAsSpecified()
+    {
+        // Arrange & Act
+        var legendaryItem = new LegendaryItem("A legendary item", 12);
+
+        // Assert
+        Assert.Equal(12, legendaryItem.SellIn);
+    }
+
+    [Fact]
+    public void LegendaryItem_Constructor_QualityIsAlways80()
+    {
+        // Arrange & Act
+        var legendaryItem = new LegendaryItem("A legendary item", 12);
+
+        // Assert
+        Assert.Equal(80, legendaryItem.Quality);
+    }
+
+    [Fact]
+    public void LegendaryItem_UpdateQuality_SellInDoesNotChange()
+    {
+        // Arrange
+        var legendaryItem = new LegendaryItem("A legendary item", 12);
+
+        // Act
+        legendaryItem.UpdateQuality();
+
+        // Assert
+        Assert.Equal(12, legendaryItem.SellIn);
+    }
+
+    [Fact]
+    public void LegendaryItem_UpdateQuality_QualityDoesNotChange()
+    {
+        // Arrange
+        var legendaryItem = new LegendaryItem("A legendary item", 12);
+        var startQuality = legendaryItem.Quality;
+
+        // Act
+        legendaryItem.UpdateQuality();
+
+        // Assert
+        Assert.Equal(startQuality, legendaryItem.SellIn);
+    }
+
+}
