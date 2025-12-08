4a459fada5aa10d62e670cde45ccb70e1ab8a5af
James Robertson <oatbake@hotmail.co.uk>
2025-12-08 04:10:30 +0000

    Added ConjuredItem

---
+++ src/GildedRose/Items/ConjuredItem.cs
@@ -0,0 +1,27 @@
+﻿namespace GildedRoseKata.Items;
+public class ConjuredItem : StoreItem
+{
+    public ConjuredItem(string name, int sellIn, int quality) : base(name, sellIn, quality)
+    {
+    }
+
+    public override void UpdateQuality()
+    {
+        SellIn -= 1;
+
+        var qualityChange = 2;
+
+        if (SellIn < 0)
+        {
+            qualityChange = 4;
+        }
+
+        Quality -= qualityChange;
+
+        //Ensure quality does not go below 0
+        Quality = Math.Max(0, Quality);
+
+        
+    }
+}
+
---
+++ src/GildedRoseTests/ConjuredItemTests.cs
@@ -0,0 +1,81 @@
+﻿using GildedRoseKata.Items;
+
+namespace GildedRoseTests;
+
+public  class ConjuredItemTests
+{
+
+    [Fact]
+    public void ConjuredItem_UpdateQuality_SellInDecrementsByOne()
+    {
+        // Arrange
+        var storeItem = new ConjuredItem("A store item", 12, 45);
+        var startSellIn = storeItem.SellIn;
+
+        // Act
+        storeItem.UpdateQuality();
+
+        // Assert
+        Assert.Equal(startSellIn - 1, storeItem.SellIn);
+    }
+
+    [Fact]
+    public void ConjuredItem_UpdateQuality_SellInPositive_QualityDecrementsByTwo()
+    {
+        // Arrange
+        var storeItem = new ConjuredItem("A store item", 1, 45);
+        var startQuality = storeItem.Quality;
+
+        // Act
+        storeItem.UpdateQuality();
+
+        // Assert
+        Assert.Equal(startQuality - 2, storeItem.Quality);
+    }
+
+
+    [Fact]
+    public void ConjuredItem_UpdateQuality_SellInZero_QualityDecrementsByFour()
+    {
+        // Arrange
+        var storeItem = new ConjuredItem("A store item", 0, 45);
+        var startQuality = storeItem.Quality;
+
+        // Act
+        storeItem.UpdateQuality();
+
+        // Assert
+        Assert.Equal(startQuality - 4, storeItem.Quality);
+    }
+
+
+    [Fact]
+    public void ConjuredItem_UpdateQuality_QualityZero_CannotGoNegative()
+    {
+        // Arrange
+        var storeItem = new ConjuredItem("A store item", 5, 0);
+        var startQuality = storeItem.Quality;
+
+        // Act
+        storeItem.UpdateQuality();
+
+        // Assert
+        Assert.Equal(0, storeItem.Quality);
+    }
+
+    [Fact]
+    public void ConjuredItem_UpdateQuality_QualityOne_OnlyGoesToZero()
+    {
+        // Arrange
+        var storeItem = new ConjuredItem("A store item", 4, 1);
+        var startQuality = storeItem.Quality;
+
+        // Act
+        storeItem.UpdateQuality();
+
+        // Assert
+        Assert.Equal(0, storeItem.Quality);
+    }
+
+}
+
