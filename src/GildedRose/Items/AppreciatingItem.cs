95a930ebbf710c46bb3e812d0ea4a87314e8ef58
James Robertson <oatbake@hotmail.co.uk>
2025-12-08 00:42:56 +0000

    Added AppreciatingItem (e.g. Brie) and associated tests

---
+++ src/GildedRose/Items/AppreciatingItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata.Items
+{
+    public class AppreciatingItem : StoreItem
+    {
+        public AppreciatingItem(string name, int sellIn, int quality) : base(name, sellIn, quality)
+        {
+        }
+
+        public new void UpdateQuality()
+        {
+            SellIn -= 1;
+
+            Quality = Math.Min(50,  Quality + 1);
+        }
+    }
+}
---
+++ src/GildedRoseTests/AppreciatingItemTests.cs
@@ -0,0 +1,34 @@
+﻿using GildedRoseKata.Items;
+
+namespace GildedRoseTests;
+
+public class AppreciatingItemTests
+{
+    [Fact]
+    public void AppreciatingItem_UpdateQuality_IncreasesByOne()
+    {
+        var item = new AppreciatingItem("Cheese", 20, 10);
+        var startQuality = item.Quality;
+        item.UpdateQuality();
+        Assert.Equal(startQuality + 1, item.Quality);
+    }
+
+    [Fact]
+    public void AppreciatingItem_UpdateQuality_AtMaximum_DoesNotIncrease()
+    {
+        var item = new AppreciatingItem("Cheese", 20, 50);
+        var startQuality = item.Quality;
+        item.UpdateQuality();
+        Assert.Equal(startQuality, item.Quality);
+    }
+
+    [Fact]
+    public void AppreciatingItem_UpdateQuality_SellInDecreasesByOne()
+    {
+        var item = new AppreciatingItem("Cheese", 20, 50);
+        var startSellIn = item.SellIn;
+        item.UpdateQuality();
+        Assert.Equal(startSellIn - 1, item.SellIn);
+    }
+}
+
